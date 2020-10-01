872f77c377a4722eace50605a6ed5fd7cf62ae7b
dantso <35578467+dantso@users.noreply.github.com>
2020-10-01 10:42:13 -0400

    Updated app.py

--- flaskApp/app.py
+++ flaskApp/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return "Hello, World!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
